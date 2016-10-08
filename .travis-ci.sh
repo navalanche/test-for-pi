6113f6c158b9ae4a5e1594d7f31df81a78696793
Erwin Gribnau <erwin@gribnau.org>
2016-10-08 11:28:23 +0200

    More Travis fixes

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,14 +3,12 @@
 if [ "${ARCH}" == "arm" ]; then
   echo "We are running an ARM build"
   docker run -it --rm -v ${PWD}:/source nineties/rust-arm cargo build --release
-  mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi target/arm-unknown-linux-gnueabihf/debug/test-for-pi-arm
+  sudo mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi target/arm-unknown-linux-gnueabihf/debug/test-for-pi-arm
 fi
 
 if [ "${ARCH}" == "x64" ]; then
   echo "We are running normal build"
   cargo build --release
   mv target/release/test-for-pi target/release/test-for-pi-x64
-  pwd
-  find .
 fi
 
