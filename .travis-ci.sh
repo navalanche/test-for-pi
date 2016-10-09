09ae14eb51e6392681054b1c9d908ca93049e9bf
Erwin Gribnau <erwin@gribnau.org>
2016-10-09 11:37:12 +0200

    More Travis fixes - 9

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,15 +2,17 @@
 
 if [ "${ARCH}" == "arm" ]; then
   echo "We are running an ARM build"
+  mkdir deploy
   docker run -it --rm -v ${PWD}:/source nineties/rust-arm cargo build --release
   whoami
-  sudo mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi target/arm-unknown-linux-gnueabihf/debug/test-for-pi-arm
-  sudo chmod a+rwx target/arm-unknown-linux-gnueabihf/debug/test-for-pi-arm
+  sudo mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi deploy/test-for-pi-arm
+  sudo chown travis.travis deploy/test-for-pi-arm
 fi
 
 if [ "${ARCH}" == "intel" ]; then
   echo "We are running normal build"
+  mkdir deploy
   cargo build --release
-  mv target/release/test-for-pi target/release/test-for-pi-intel
+  mv target/release/test-for-pi deploy/test-for-pi-intel
 fi
 
