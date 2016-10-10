d8f9143c006d331ebf38866dbfb2f1e79839cab7
Erwin Gribnau <erwin@gribnau.org>
2016-10-10 15:46:08 +0200

    Fix ARMv7 cargo build command

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,7 +6,7 @@ cargo build --release
 mv target/release/test-for-pi deploy/test-for-pi-intel
 
 echo "ARMv7 build:"
-docker run -it --rm -v ${PWD}:/work rustcross/armv7-unknown-linux-gnueabihf:latest cargo build --release
+docker run -it --rm -v ${PWD}:/work rustcross/armv7-unknown-linux-gnueabihf:latest cargo build --release --target armv7-unknown-linux-gnueabihf
 sudo mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi deploy/test-for-pi-armv7
 sudo chown travis.travis deploy/test-for-pi-armv7
 
