33499c0d86666758a2d543717689dc557b53276d
Erwin Gribnau <erwin@gribnau.org>
2016-10-10 15:43:08 +0200

    Fix ARMv7 deliverable name

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,7 +7,6 @@ mv target/release/test-for-pi deploy/test-for-pi-intel
 
 echo "ARMv7 build:"
 docker run -it --rm -v ${PWD}:/work rustcross/armv7-unknown-linux-gnueabihf:latest cargo build --release
-whoami
-sudo mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi deploy/test-for-pi-arm
-sudo chown travis.travis deploy/test-for-pi-arm
+sudo mv target/arm-unknown-linux-gnueabihf/debug/test-for-pi deploy/test-for-pi-armv7
+sudo chown travis.travis deploy/test-for-pi-armv7
 
